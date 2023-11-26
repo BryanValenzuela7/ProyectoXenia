37b51397159e6dc76a1489d6afc914a0e6803613
ELIAN-OSORIO <al19760601@ite.edu.mx>
2023-11-25 22:31:24 -0800

    Ajustes-Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,12 @@
 "use client";
 import React from "react";
 import "../app/styles/styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faUser,
+  faUserCog,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Formulario = () => {
   return (
@@ -9,41 +15,44 @@ const Formulario = () => {
         <div className="text-center text-white pb-8 text-2xl">
           <h1>Registro de personal</h1>
         </div>
-        <div className="flex-1 w-64 mb-4 mx-8 input-container">
+        <div className="flex-1 w-64 mb-4 mx-8">
           <label htmlFor="responsable">
-            <p className="text-white p-2 text-lg">Encargado del programa</p>
+            <p className="text-white p-2 text-lg">
+              <FontAwesomeIcon icon={faUser} /> Encargado del programa
+            </p>
           </label>
           <input
             type="text"
             id="responsable"
             placeholder="Responsable"
-            className="bg-slate-100 p-2 rounded-md w-full image-input"
+            className="bg-slate-100 text-black p-2 rounded-md w-full"
           />
-          <img src='/Image/user.png' className='input-image'/>
         </div>
-        <div className="flex-1 w-64 mb-4 mx-8 input-container">
+        <div className="flex-1 w-64 mb-4 mx-8">
           <label htmlFor="rol">
-            <p className="text-white p-2 text-lg">Rol del responsable</p>
+            <p className="text-white p-2 text-lg">
+              <FontAwesomeIcon icon={faUserCog} /> Rol del responsable
+            </p>
           </label>
           <input
             type="text"
             id="rol"
             placeholder="Puesto"
-            className="bg-slate-100 p-2 rounded-md w-full image-input"
+            className="bg-slate-100 text-black p-2 rounded-md w-full"
           />
-          <img src='/Image/maletin.png' className='input-image'/>
         </div>
-        <div className="flex-1 w-64 mb-4 mx-8 input-container">
+        <div className="flex-1 w-64 mb-4 mx-8">
           <label htmlFor="correo">
-            <p className="text-white p-2 text-lg">Correo del responsable</p>
+            <p className="text-white p-2 text-lg">
+              <FontAwesomeIcon icon={faEnvelope} /> Correo del responsable
+            </p>
           </label>
           <input
             type="text"
             id="correo"
             placeholder="Contacto"
-            className="bg-slate-100 p-2 rounded-md w-full image-input"
+            className="bg-slate-100 text-black p-2 rounded-md w-full"
           />
-          <img src="/Image/mail.png" className='input-image'/>
         </div>
         <button className="bg-gray-800 text-white mx-10 my-2 px-10 py-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-gray-800">
           Registrar
