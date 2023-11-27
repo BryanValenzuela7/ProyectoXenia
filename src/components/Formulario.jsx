d146dec9e1ad6c3456aaccbd1ed9ec91c984762f
ELIAN-OSORIO <al19760601@ite.edu.mx>
2023-11-26 17:45:03 -0800

    Reajuste-Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,63 +1,66 @@
 "use client";
 import React from "react";
-import "../app/styles/styles.css";
+/* import "../app/styles/styles.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faEnvelope,
   faUser,
   faUserCog,
 } from "@fortawesome/free-solid-svg-icons";
-
+ */
+import { useState } from "react";
+import Registrodependencia from "./Registrodependencia";
+import Registropersonal from "./Registropersonal";
 const Formulario = () => {
+  const [page, setPage] = useState(0);
+  const [formData, setFormData] = useState({
+    encargado: "",
+    rol: "",
+    correo: "",
+    dependencia: "",
+    direccion: "",
+    contacto: "",
+    propietario: "",
+    ocupacion: "",
+    seccion: "",
+  });
+  /* const FormTitles = ["Registro de personal", "Dependencia"]; */
+  const PageDisplay = () => {
+    if (page === 0) {
+      return <Registropersonal formData={formData} setFormData={setFormData} />;
+    } else if (page === 1) {
+      return (
+        <Registrodependencia formData={formData} setFormData={setFormData} />
+      );
+    }
+  };
   return (
-    <div className="flex justify-center m-8">
-      <form className="flex flex-col sm:grid-cols-2 gap-4 place-content-stretch py-8 formulario-con-sombra rounded-md">
-        <div className="text-center text-white pb-8 text-2xl">
-          <h1>Registro de personal</h1>
+    <div className="form">
+      <div className="progressbar"></div>
+      <div className="form-container">
+        {/* <div className="header">
+          <h1>{FormTitles[page]}</h1>
+        </div> */}
+        <div className="body">{PageDisplay()}</div>
+        <div className="footer">
+          <button
+            className="bg-gray-800 text-white mx-10 my-2 px-10 py-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-gray-800"
+            onClick={() => {
+              setPage((currPage) => currPage - 1);
+            }}
+          >
+            Prev
+          </button>
+          <button
+            className="bg-gray-800 text-white mx-10 my-2 px-10 py-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-gray-800"
+            onClick={() => {
+              setPage((currPage) => currPage + 1);
+            }}
+          >
+            Next
+          </button>
         </div>
-        <div className="flex-1 w-64 mb-4 mx-8">
-          <label htmlFor="responsable">
-            <p className="text-white p-2 text-lg">
-              <FontAwesomeIcon icon={faUser} /> Encargado del programa
-            </p>
-          </label>
-          <input
-            type="text"
-            id="responsable"
-            placeholder="Responsable"
-            className="bg-slate-100 text-black p-2 rounded-md w-full"
-          />
-        </div>
-        <div className="flex-1 w-64 mb-4 mx-8">
-          <label htmlFor="rol">
-            <p className="text-white p-2 text-lg">
-              <FontAwesomeIcon icon={faUserCog} /> Rol del responsable
-            </p>
-          </label>
-          <input
-            type="text"
-            id="rol"
-            placeholder="Puesto"
-            className="bg-slate-100 text-black p-2 rounded-md w-full"
-          />
-        </div>
-        <div className="flex-1 w-64 mb-4 mx-8">
-          <label htmlFor="correo">
-            <p className="text-white p-2 text-lg">
-              <FontAwesomeIcon icon={faEnvelope} /> Correo del responsable
-            </p>
-          </label>
-          <input
-            type="text"
-            id="correo"
-            placeholder="Contacto"
-            className="bg-slate-100 text-black p-2 rounded-md w-full"
-          />
-        </div>
-        <button className="bg-gray-800 text-white mx-10 my-2 px-10 py-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-gray-800">
-          Registrar
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
---
+++ src/components/Registropersonal.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "../app/styles/styles.css";
+
+function Registropersonal({ formData, setFormData }) {
+  return (
+    <div className="flex justify-center m-8">
+      <form className="flex flex-col sm:grid-cols-2 gap-4 place-content-stretch py-8 formulario-con-sombra rounded-md">
+        <div className="text-center text-white pb-8 text-2xl">
+          <h1>Registro de personal</h1>
+        </div>
+        <div className="flex-1 w-64 mb-4 mx-8">
+          <label htmlFor="responsable">
+            <p className="text-white p-2 text-lg"> Encargado del programa</p>
+          </label>
+          <input
+            type="text"
+            id="responsable"
+            placeholder="Responsable"
+            value={formData.encargado}
+            onChange={(event) =>
+              setFormData({ ...formData, encargado: event.target.value })
+            }
+            className="bg-slate-100 text-black p-2 rounded-md w-full"
+          />
+        </div>
+        <div className="flex-1 w-64 mb-4 mx-8">
+          <label htmlFor="rol">
+            <p className="text-white p-2 text-lg">Rol del responsable</p>
+          </label>
+          <input
+            type="text"
+            id="rol"
+            placeholder="Puesto"
+            value={formData.rol}
+            onChange={(event) =>
+              setFormData({ ...formData, rol: event.target.value })
+            }
+            className="bg-slate-100 text-black p-2 rounded-md w-full"
+          />
+        </div>
+        <div className="flex-1 w-64 mb-4 mx-8">
+          <label htmlFor="correo">
+            <p className="text-white p-2 text-lg">Correo del responsable</p>
+          </label>
+          <input
+            type="text"
+            id="correo"
+            placeholder="Contacto"
+            value={formData.correo}
+            onChange={(event) =>
+              setFormData({ ...formData, correo: event.target.value })
+            }
+            className="bg-slate-100 text-black p-2 rounded-md w-full"
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Registropersonal;
